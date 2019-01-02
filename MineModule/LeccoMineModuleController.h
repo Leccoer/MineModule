24c8ab54ee633789dac80bbfbb863e9f52fc2932
lecco <10915819@qq.com>
2019-01-02 17:19:31 +0800

    first commit

---
+++ MineModule/LeccoMineModuleController.h
@@ -0,0 +1,17 @@
+//
+//  LeccoMineModuleController.h
+//  MineModuleDemo
+//
+//  Created by yixin on 2019/1/2.
+//  Copyright © 2019 lecco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LeccoMineModuleController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MineModule/MineModule.h
@@ -0,0 +1,13 @@
+//
+//  MineModule.h
+//  MineModuleDemo
+//
+//  Created by yixin on 2019/1/2.
+//  Copyright © 2019 lecco. All rights reserved.
+//
+
+#ifndef MineModule_h
+#define MineModule_h
+
+
+#endif /* MineModule_h */
