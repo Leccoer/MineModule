880796390a218d3593164f6d400b1ab2940fbe2e
lecco <10915819@qq.com>
2019-01-02 18:01:35 +0800

    [fix] 修改

--- MineModule/MineModule.h
+++ MineModule/MineModule.h
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-#import <MIn>
-
 //! Project version number for MineModule.
 FOUNDATION_EXPORT double MineModuleVersionNumber;
 
--- MineModuleDemo/ViewController.h
+++ MineModuleDemo/ViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MineModule/LeccoMineModuleController.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : LeccoMineModuleController
 
 
 @end
