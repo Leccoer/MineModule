99666c7b4fedf41012a70f54522bf1157a6fb412
lecco <10915819@qq.com>
2019-01-02 16:53:26 +0800

    Initial Commit

---
+++ MineModuleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MineModuleDemo
+//
+//  Created by yixin on 2019/1/2.
+//  Copyright © 2019 lecco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MineModuleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MineModuleDemo
+//
+//  Created by yixin on 2019/1/2.
+//  Copyright © 2019 lecco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
